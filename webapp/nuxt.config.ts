4fca509e29dd3beeb670349eb58a964b1a1f285c
Abdallah Slimane <slimane.abdallah75@gmail.com>
2025-05-28 14:07:27 +0200

    dockerise services

--- webapp/nuxt.config.ts
+++ webapp/nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: 'http://localhost:8080/api', // À adapter
+      apiBase: 'http://localhost:8080', // À adapter
     }
   },
   modules: [
