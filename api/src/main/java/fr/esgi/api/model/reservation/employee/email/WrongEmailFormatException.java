89128fa4a815290e02f6f04f1a70bdf70cae1c58
melyLaur <melissanoemi.laurent@free.fr>
2025-05-30 21:59:20 +0200

    put english message

--- api/src/main/java/fr/esgi/api/model/reservation/employee/email/WrongEmailFormatException.java
+++ api/src/main/java/fr/esgi/api/model/reservation/employee/email/WrongEmailFormatException.java
@@ -3,7 +3,7 @@ package fr.esgi.api.model.reservation.employee.email;
 import fr.esgi.api.model.DomainException;
 
 public final class WrongEmailFormatException extends DomainException {
-    private static final String MESSAGE = "Le format de l'email est invalide";
+    private static final String MESSAGE = "Email format not valid.";
 
     public WrongEmailFormatException() {
         super(MESSAGE);
--- api/src/main/java/fr/esgi/api/model/reservation/exceptions/ReservationNotFoundException.java
+++ api/src/main/java/fr/esgi/api/model/reservation/exceptions/ReservationNotFoundException.java
@@ -3,7 +3,7 @@ package fr.esgi.api.model.reservation.exceptions;
 import fr.esgi.api.model.DomainException;
 
 public class ReservationNotFoundException extends DomainException {
-    private static final String MESSAGE = "La réservation n'existe pas.";
+    private static final String MESSAGE = "Reservation not found";
     public ReservationNotFoundException() {
         super(MESSAGE);
     }
